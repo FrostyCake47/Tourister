0a2ad38eb32334cfc856cc6e3290e0f63dddbe88
FrostyCake47 <47393739+FrostyCake47@users.noreply.github.com>
2023-08-06 18:12:01 +0530

    Added slash command functionality & keyword search

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import googlemaps
 import os
 import discord
+from discord import app_commands
+import requests
+import asyncio
 
 TOKEN = os.environ['TOURISTER_SECRET']
 API_KEY = os.environ["GMAPS_API_KEY"]
 print(TOKEN)
 
 client = discord.Client(command_prefix='/', intents=discord.Intents().all())
+tree = app_commands.CommandTree(client)
 
 def location_to_coordinates(gmaps, location):
     geocode_result = gmaps.geocode(location)
@@ -36,6 +40,26 @@ def get_place_main_photo(api_key, place_id, max_width=400):
     url = f"{base_url}?{max_width_param}&{place_id_param}&{api_key_param}"
     return url
 
+def get_pic_url(api_key, place_id):
+    place_details = gmaps.place(place_id=place_id, fields=['photo'])
+    photo_id = place_details['result']['photos'][0]['photo_reference']
+    print("photo id=", photo_id)
+
+    base_url = "https://maps.googleapis.com/maps/api/place/photo"
+    params = {
+        "key": api_key,
+        "photoreference": photo_id,
+        "maxheight": 400
+    }
+    response = requests.get(base_url, params=params)
+
+    if response.status_code == 200:
+        return response.url
+    else:
+        print("Error:", response.status_code, response.text)
+        return None
+
+
 def get_road_distance(origin, destination, mode="driving"):
     try:
         result = gmaps.distance_matrix(
@@ -59,18 +83,29 @@ def get_road_distance(origin, destination, mode="driving"):
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+    await tree.sync(guild=discord.Object(id=1137264222603059300))
+    
 
-
-@client.event
-async def on_message(message):
-    KEYWORD = 'Popular places'
-    RADIUS = 10000
+@tree.command(name="test", description='description',guild=discord.Object(id=1137264222603059300))
+async def test(interaction: discord.Interaction):
     try:
-        if message.author == client.user:
-            return
-        
-        if message.content.startswith("/popular"):
-            place = str(message.content).split("/popular")[1]
+        await interaction.response.send_message("omg")
+    
+    except Exception as e:
+        print(e)
+
+
+#@client.event
+#async def on_message(message):
+
+@tree.command(name="locate", description='Location',guild=discord.Object(id=1137264222603059300))
+async def locate(message: discord.Interaction, location: str, keyword :str, search_radius: int):
+
+    RADIUS = search_radius
+    KEYWORD = keyword
+    try:
+        if message:
+            place = location
             LOCATION = location_to_coordinates(gmaps, place)
             print(LOCATION)
             popular_areas = get_popular_areas(LOCATION, RADIUS, KEYWORD)
@@ -90,18 +125,22 @@ async def on_message(message):
                     distance = 'NULL'
                 
                 print("distance = ", distance)
+                image_url = get_pic_url(API_KEY, place_id)
+                print(url)
 
                 embed=discord.Embed(title=f"{i+1}) {place['name']}", description=place['formatted_address'], color=0x772eff, url=url )
                 embed.add_field(name="Rating", value=f"{place['rating']} ⭐ ({place['user_ratings_total']})", inline=False)
                 embed.add_field(name="Distance", value=f"{round(distance/1000, 1)} km", inline=False)
                 embed.set_image(url=image_url)
+
                 await message.channel.send(embed=embed)
-                if i == 4:
+                
+                if i == 1:
                     break
 
 
     except Exception as e:
-        print(e)
+        print("error is = ", e)
         await message.channel.send("ayo something went wrong, try again")
 
 
